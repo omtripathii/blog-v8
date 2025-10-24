583b05035ec2dd2f432bb873f1ad5108e8e220db
Om Tripathi <tripathi111om@gmail.com>
2025-10-25 01:28:38 +0530

    ignored ts errors

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Avatar } from "./BlogCard";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
